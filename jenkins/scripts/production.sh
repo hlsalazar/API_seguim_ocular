8881be1ff09701274a637ce5807e09e3131bdf1a
KevinM97 <khmina@espe.edu.ec>
2024-02-26 18:33:07 -0500

    Cambios en sh de jenkins

--- jenkins/scripts/production.sh
+++ jenkins/scripts/production.sh
@@ -1,1 +1,2 @@
+firebase use --add espe-eyetracker
 firebase deploy -P espe-eyetracker --token "$FIREBASE_DEPLOY_TOKEN"
--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,2 +1,3 @@
 npm install -g firebase-tools
+firebase use --add espe-eyetracker-test
 firebase deploy -P espe-eyetracker-test --token "$FIREBASE_DEPLOY_TOKEN"
