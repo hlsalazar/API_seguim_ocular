3bd470600f6e5a1b8086b3a1709d91362536235c
hlsalazar <hlsalazar@espe.edu.ec>
2024-07-10 15:19:57 -0500

    firebase lectura 2

--- api/data.js
+++ api/data.js
@@ -1,11 +1,28 @@
-import { ref, get } from 'firebase/database';
-import { db } from '../firebaseConfig.js';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
-export default async function handler(req, res) {
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDXj7QuX8r4aVvRbuOljxrY26kZLpP8KFM",
+  authDomain: "registroocularuso.firebaseapp.com",
+  databaseURL: "https://registroocularuso-default-rtdb.firebaseio.com",
+  projectId: "registroocularuso",
+  storageBucket: "registroocularuso.appspot.com",
+  messagingSenderId: "230219267012",
+  appId: "1:230219267012:web:7b8989887687bc16c78291",
+  measurementId: "G-2FVK8VQHN4"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+export default async (req, res) => {
   if (req.method === 'GET') {
     try {
-      const dataRef = ref(db, 'path/to/your/data');
-      const snapshot = await get(dataRef);
+      const snapshot = await get(ref(db, 'arrayGuardar')); // Cambia este camino por el de tus datos en la base de datos
       const data = snapshot.val();
       res.status(200).json(data);
     } catch (error) {
@@ -15,4 +32,4 @@ export default async function handler(req, res) {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-}
+};
